2cc11863fab7e5783b55991ac711d27150035a7b
dakl <dakl@itu.dk>
2025-10-23 12:03:31 +0200

    fixed message input into server

--- Client/client.go
+++ Client/client.go
@@ -2,64 +2,67 @@ package main
 
 import (
 	proto "Chit-Chat/gRPC"
+	"bufio"
 	"context"
 	"log"
+	"os"
 	"os/user"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"fmt"
 )
 
-func main(){
-    conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials()))
+func main() {
+	conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Not working client 1")
 	}
 
-    currentUser, err := user.Current()
-    if err != nil {
-        log.Fatalf(err.Error())
-    }
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 
 	client := proto.NewChitChatClient(conn)
 	username := currentUser.Username
 	uid := currentUser.Uid
-		
-	join, err := client.JoinChit(context.Background(), 
+
+	join, err := client.JoinChit(context.Background(),
 		&proto.JoinRequest{
 			Author: &proto.Author{
-				Id: uid,
+				Id:   uid,
 				Name: username,
 			},
 		})
 	if err != nil {
 		log.Fatalf("Not working client 2")
 	}
-	
+
 	log.Println(join)
 	send(client, username)
 }
 
-func send(client proto.ChitChatClient, username string){
-	for{
-		var command string
-		fmt.Scanln(&command)
-		if (command == "Leave"){
+func send(client proto.ChitChatClient, username string) {
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		s := scanner.Text()
+		if s == "Leave" {
 			break
 		} else {
 			send, err := client.SendChits(context.Background(),
-			&proto.Chit{
-				Chit: command,
-				Author: username,
-				TimeFormated: "your mom",
-			},
-		)
-		if err != nil {
-			log.Fatalf("client not sending message")
-		}
+				&proto.Chit{
+					Chit:         s,
+					Author:       username,
+					TimeFormated: "your mom",
+				},
+			)
+			if err != nil {
+				log.Fatalf("client not sending message")
+			}
 
-		log.Println(send)
-			
+			log.Println(send)
+
 		}
 	}
 }
